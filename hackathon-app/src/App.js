8b247107fd938a8c719e534f498ec272bff855f0
karanpatil41 <karanpatil414141@gmail.com>
2024-02-21 07:47:29 +0530

    First commit with Project

---
+++ hackathon-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <h1>Hello Madam....Let's HACK!!!</h1>
+    </div>
+  );
+}
+
+export default App;
