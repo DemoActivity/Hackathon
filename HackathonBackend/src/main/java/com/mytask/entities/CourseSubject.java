f84e44a1b8b5d2409246aedbb1b37d3ad0919a15
karanpatil41 <karanpatil414141@gmail.com>
2024-02-23 11:24:31 +0530

    Added entities

--- HackathonBackend/src/main/java/com/mytask/entities/CourseSubject.java
+++ HackathonBackend/src/main/java/com/mytask/entities/CourseSubject.java
@@ -1,5 +1,38 @@
 package com.mytask.entities;
 
-public class CourseSubject {
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "course_subject")
+public class CourseSubject {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cs_id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "course_id")
+	private Course course;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "subject_id")
+	private Subject subject;
 }
--- HackathonBackend/src/main/java/com/mytask/entities/EvaluationSchedule.java
+++ HackathonBackend/src/main/java/com/mytask/entities/EvaluationSchedule.java
@@ -1,5 +1,37 @@
 package com.mytask.entities;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "evaluation_schedule")
 public class EvaluationSchedule {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "subject_id")
+	private Subject subject;
+	private String evaluationType;
+	private LocalDate validTill;
+	private String group;
+	private Long userId;
 }
